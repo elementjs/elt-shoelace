22e7302a45edece1ae3305dca219a5e33256ba10
Christophe Eymard <christophe.eymard@sales-way.com>
2023-11-29 19:41:38 +0100

    $model now properly binds on <sl-menu-item>

--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,8 @@
 import { node_add_event_listener, node_observe, o } from "elt"
-import type { SlInput } from "@shoelace-style/shoelace"
+import type { SlInput, SlSelectEvent } from "@shoelace-style/shoelace"
 import { SlElement } from "./components/_monkey"
+import { node_on_connected } from "elt"
+import { node_on_disconnected } from "elt"
 
 /**
  * Binds an observable to a Node
@@ -65,37 +67,69 @@ export function $model(ob: o.RO<any>, unfocused_fn?: (v: string) => string): any
           })
         }
 
-        node_add_event_listener(node, "sl-input", () => {
-          lock(() => {
-            let nval
-            switch (node.tagName) {
-              case "SL-SWITCH":
-              case "SL-CHECKBOX":
-              case "SL-MENU-ITEM":
-                nval = node.checked
-                break
-              case "SL-INPUT":
-                if (node.tagName === "SL-INPUT" && node.type === "number") {
-                  nval = node.valueAsNumber
-                } else {
-                  nval = node.value
-                }
-                break
-              case "SL-SELECT":
-                nval = node.value
-                if (node.multiple) {
-                  ob.set(Array.isArray(nval) ? nval : [nval])
-                } else {
-                  ob.set(nval)
-                }
-                return
-              default:
-                nval = node.value
-                break
-              }
-              ob.set(Array.isArray(nval) ? nval[0] : nval)
+        // menu-item is a bitch and needs to be bound to the menu item itself.
+        if (node.tagName === "SL-MENU-ITEM") {
+          let menu: HTMLElement | null = null
+
+          function _menu_from_event(ev: SlSelectEvent) {
+            lock(() => {
+              ob.set(ev.detail.item.checked)
+            })
+          }
+
+          node_on_connected(node, node => {
+
+            menu = node.parentElement
+            while (menu != null && menu.tagName !== "SL-MENU") {
+              console.log(menu.tagName)
+              menu = menu.parentElement
+            }
+
+            if (menu == null) {
+              console.warn("trying to bind on an sl-menu-item without sl-menu")
+              return
+            }
+
+            menu.addEventListener("sl-select", _menu_from_event)
           })
-        })
+
+          node_on_disconnected(node, () => {
+            menu?.removeEventListener("sl-select", _menu_from_event)
+          })
+
+        } else {
+          node_add_event_listener(node, "sl-input", () => {
+            lock(() => {
+              let nval
+              switch (node.tagName) {
+                case "SL-SWITCH":
+                case "SL-CHECKBOX":
+                  nval = node.checked
+                  break
+                case "SL-INPUT":
+                  if (node.tagName === "SL-INPUT" && node.type === "number") {
+                    nval = node.valueAsNumber
+                  } else {
+                    nval = node.value
+                  }
+                  break
+                case "SL-SELECT":
+                  nval = node.value
+                  if (node.multiple) {
+                    ob.set(Array.isArray(nval) ? nval : [nval])
+                  } else {
+                    ob.set(nval)
+                  }
+                  return
+                default:
+                  nval = node.value
+                  break
+                }
+                ob.set(Array.isArray(nval) ? nval[0] : nval)
+            })
+          })
+        }
+
       }
     }
   }
